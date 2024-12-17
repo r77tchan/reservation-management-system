5d99e62e52de52c1c2506c7c04f6c6803fea175a
r77tchan <r77tchan@gmail.com>
2024-12-17 18:51:38 +0900

    データベース接続処理を実装

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,15 @@
-// インポートされると接続を実行するファイル
+// requireされると接続を実行するファイル
 
 const mysql = require('mysql2')
 const dotenv = require('dotenv')
 
 // .envファイルの内容を読み込み、環境変数 (process.env) に設定
-dotenv.config({ path: __dirname + '../.env' })
+dotenv.config({ path: __dirname + '/../.env' })
+
+console.log('DB_HOST:', process.env.DB_HOST)
+console.log('DB_USER:', process.env.DB_USER)
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD)
+console.log('DB_NAME:', process.env.DB_NAME)
 
 // 接続準備
 const connection = mysql.createConnection({
