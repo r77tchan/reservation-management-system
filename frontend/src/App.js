eaa80d4bb702cd208aaf7e46d3f7b5ce2e84e1f1
r77tchan <r77tchan@gmail.com>
2024-12-17 17:58:36 +0900

    frontでルーティングを設定し、ページ間の遷移を実装

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,38 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Default from './pages/Default'
+import Top from './pages/Top'
+import View from './pages/View'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Router>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Top</Link>
+            </li>
+            <li>
+              <Link to="/view">View</Link>
+            </li>
+            <li>
+              <Link to="/Register">register</Link>
+            </li>
+            <li>
+              <Link to="/Login">login</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
         <Route path="/default" element={<Default />} />
+        <Route path="/" element={<Top />} />
+        <Route path="/view" element={<View />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
---
+++ frontend/src/pages/Top.js
@@ -0,0 +1,7 @@
+import '../my.css'
+
+function Top() {
+  return <h1>ようこそ、予約管理システムへ</h1>
+}
+
+export default Top
