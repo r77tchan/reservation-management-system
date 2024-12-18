185c85ac65c9ea60910a861b5494cf7f31475b17
r77tchan <r77tchan@gmail.com>
2024-12-18 14:17:38 +0900

    corsを使用

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 // ルート設定
 const indexRouter = require('./routes/index')
@@ -21,6 +22,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
