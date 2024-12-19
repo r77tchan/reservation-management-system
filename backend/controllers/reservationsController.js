8f60794e339db617fcec3b74c807f5726be02787
r77tchan <r77tchan@gmail.com>
2024-12-19 14:52:34 +0900

    サーバー側予約作成処理実装

--- backend/controllers/reservationsController.js
+++ backend/controllers/reservationsController.js
@@ -3,7 +3,7 @@ const Reservation = require('../models/Reservation')
 // 送信ユーザーの予約を取得
 exports.list = async (req, res) => {
   try {
-    // JWTからユーザー情報を取得（middlewareでデコード済みのデータが req.user に入っていると仮定）
+    // JWTからユーザー情報を取得 // ミドルウェアでデコードされた JWT のuserId
     const userId = req.user.userId
 
     // 特定ユーザーの予約を取得
@@ -18,7 +18,25 @@ exports.list = async (req, res) => {
 }
 
 // 予約新規作成
-exports.create = async (req, res) => {}
+exports.create = async (req, res) => {
+  try {
+    // リクエストボディからデータを取得
+    const { date, time, status } = req.body
+    // ミドルウェアでデコードされた JWT のuserId
+    const userId = req.user.userId
+
+    if (!date || !time || !status) {
+      return res.status(400).json({ error: '全てのフィールドを入力してください。' })
+    }
+
+    // 新しい予約を挿入
+    await Reservation.insertReservation({ user_id: userId, date, time, status })
+
+    res.status(201).json({ message: '予約作成成功' })
+  } catch (err) {
+    res.status(500).json({ error: '予約作成中にエラーが発生しました', details: err.message })
+  }
+}
 
 // 予約編集
 exports.update = async (req, res) => {}
